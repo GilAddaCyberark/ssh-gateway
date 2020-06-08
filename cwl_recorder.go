ba1e3c05887bd84b3899fad86a4094a9986018ef
gadda <gil.adda@cyberark.com>
2020-06-08 13:33:13 +0300

    logger to cwl fix

--- cwl_recorder.go
+++ cwl_recorder.go
@@ -2,8 +2,13 @@ package main
 
 import (
 	"bytes"
+	aws_helpers "ssh-gateway/aws_workers"
 	cwlogger "ssh-gateway/cw_logger"
 	"time"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/cloudwatchlogs"
 )
 
 type CWLRecorder struct {
@@ -13,8 +18,15 @@ type CWLRecorder struct {
 
 func NewCWLRecorder() (*CWLRecorder, error) {
 	cwl := CWLRecorder{}
-	cwl.Logger = &cwlogger.Logger{}
-
+	// cwl.Logger = &cwlogger.Logger{}
+	logger, err := cwlogger.New(&cwlogger.Config{
+		LogGroupName: "SSH_Gateway_Logs",
+		Client:       cloudwatchlogs.New(session.New(), &aws.Config{Region: aws.String(aws_helpers.DEFAULT_REGION)}),
+	})
+	if err != nil {
+		return nil, err
+	}
+	cwl.Logger = logger
 	return &cwl, nil
 }
 func (c *CWLRecorder) Init() error {
@@ -25,18 +37,15 @@ func (c *CWLRecorder) Close() error {
 
 }
 func (c *CWLRecorder) Write(data []byte) error {
-	if c.logBuffer == nil {
-		c.logBuffer = bytes.NewBuffer(data)
-	} else {
-		c.logBuffer.Write(data)
-	}
-	//c.Logger.Log(time.Now(), strings.data)
-
-	spliter := []byte("]0;") // Commnad splitter instead of cariage return
-	for index := (bytes.Index(c.logBuffer.Bytes(), spliter)); index > 0; index = (bytes.Index(c.logBuffer.Bytes(), spliter)) {
-		c.Logger.Log(time.Now(), c.logBuffer.String())
-		c.logBuffer.Truncate(0)
-	}
+	// if c.logBuffer == nil {
+	// 	c.logBuffer = bytes.NewBuffer(data)
+	// } else {
+	// 	c.logBuffer.Write(data)
+	// }
+	// s := c.logBuffer.String()
+	// fmt.Printf(string(data))
+	c.Logger.Log(time.Now(), string(data))
+	// c.logBuffer.Truncate(0)
 
 	return nil
 
